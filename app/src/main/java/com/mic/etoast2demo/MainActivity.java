2991f516c16aa26d603a07d5c655b85dace151ef
zhaocheng <zhaocheng>
2017-06-30 16:45:03 +0800

    可以试试了~

--- app/src/main/java/com/mic/etoast2demo/MainActivity.java
+++ app/src/main/java/com/mic/etoast2demo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.mic.etoast2demo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.mic.etoast2.EToast2;
@@ -9,7 +9,7 @@ import com.mic.etoast2.Toast;
 
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"dadasdasdas"+new Date().getTime(), EToast2.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"这是一个不管怎么样都能显示的Toast"+new Date().getTime(), EToast2.LENGTH_SHORT).show();
             }
         });
     }
